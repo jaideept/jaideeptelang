77c3ea2910b3b463a22442559211df7d989eadd9
Jaideep <jaideeptelang@gmail.com>
2019-12-09 03:59:07 +0530

    Update Contact.cs

--- ContactApi/Repositories/Contacts/Contact.cs
+++ ContactApi/Repositories/Contacts/Contact.cs
@@ -1,16 +1,22 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
+using System;
+using System.ComponentModel.DataAnnotations;
 
-namespace ContactApi
+namespace ContactApi.Models
 {
     public class Contact : BaseModel
     {
+        [Required(ErrorMessage = "First Name is required"), MaxLength(150)]
         public string FirstName { get; set; }
+        
+        [Required(ErrorMessage = "Last Name is required"), MaxLength(150)]
         public string LastName { get; set; }
+        
+        [Required(ErrorMessage = "Email address is required"), MaxLength(150), EmailAddress]
         public string Email { get; set; }
+        
+        [Required(ErrorMessage = "Phone number is required"), MaxLength(10), Phone]
         public string Phone { get; set; }
+       
         public bool Status { get; set; }
     }
 }
