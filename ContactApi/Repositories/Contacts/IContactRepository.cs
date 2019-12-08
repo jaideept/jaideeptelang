917b481b5440c8897cd5d57388c32ef70707fae7
Jaideep <jaideeptelang@gmail.com>
2019-12-09 04:03:18 +0530

    Update IContactRepository.cs

--- ContactApi/Repositories/Contacts/IContactRepository.cs
+++ ContactApi/Repositories/Contacts/IContactRepository.cs
@@ -1,13 +1,14 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
 using System.Threading.Tasks;
 using DataAccess;
+using ContactApi.Models;
+using System.Collections.Generic;
 
-namespace ContactApi
+namespace ContactApi.Repository
 {
     public interface IContactRepository : IGenericRepository<Contact>
     {
         Task<bool> MyCustomRepositoryMethodExampleAsync();
+        Task<IEnumerable<TEntity>> GetByPageIndex<TEntity>(int page, int pageSize);
+
     }
 }
